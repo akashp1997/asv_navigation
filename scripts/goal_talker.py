6a707f3369ff45445f1064423206c0b784b31364
Akash Purandare <akash.p1997@gmail.com>
2018-05-24 19:51:08 +0530

    Stashing commits before modifying local system

--- scripts/goal_talker.py
+++ scripts/goal_talker.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import rospy
 import geometry_msgs.msg
+import tf
 
 rospy.init_node("fake_goal")
 pub = rospy.Publisher("/move_base_simple/goal", geometry_msgs.msg.PoseStamped, queue_size=10)
 msg = geometry_msgs.msg.PoseStamped()
 msg.header.frame_id = "/map"
-msg.pose.position.x = 2
-msg.pose.position.y = 2
-msg.pose.orientation.w = 1
+msg.pose.position.x = 0.5
+msg.pose.position.y = 0
+ori = tf.transformations.quaternion_from_euler(0,0,2)
+msg.pose.orientation.x = ori[0]
+msg.pose.orientation.y = ori[1]
+msg.pose.orientation.z = ori[2]
+msg.pose.orientation.w = ori[3]
 while not rospy.is_shutdown():
 	msg.header.stamp = rospy.Time.now()
 	pub.publish(msg)
